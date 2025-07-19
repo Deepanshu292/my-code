fb3c085f703754ed8df83acbf9d9b085b8553c45
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-07-19 10:20:41 +0530

    adding a size function in program of linked list

--- LL1.java
+++ LL1.java
@@ -1,5 +1,10 @@
 class LL {
     Node head;
+    private int size;
+    LL(){
+        this.size =0;
+    }
+    
     class Node{
         String data;
         Node.next;
@@ -16,6 +21,7 @@ class LL {
         } else {
             newNode.next = head;
             head = newNode;
+            size++
         }
     }
     public void addLast(String data){
@@ -40,6 +46,37 @@ class LL {
         }
         Sysout.println("NULL");
     }
+    public void deleteFirst(){
+        if(head == null){
+            System.out.println("list is empty");
+            return;
+
+        }
+        size--;
+        head =head.next;
+    }
+    public void deteLast(){
+        if(head == null){
+            System.out.println("list is empty");
+            return;
+        }
+        size--;
+        if(head.next == null){
+            head = null;
+            return;
+        }
+        Node secondLast =head;
+        Node lastNode =head.next;
+        while(lastNode.next!=null){
+            lastNode =lastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next =null;
+
+    }
+      public int getSize() {
+        return size;
+    }
     public static void main(String[] args) {
         LL list =new LL();
         list.addFirst("Hello");
@@ -47,7 +84,13 @@ class LL {
         list.printList();
         list.addLast("!");
         list.printList();
+        list.deleteFirst();
+        list.printList();
+        list.deteLast();    
+        list.printList();
+        System.out.println("Size of the list: " + list.getSize());
         
     }
+  
     
 }
