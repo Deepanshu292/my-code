78db7b1800c21b514c11ef9fe9d3dbdba3a47673
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-07-23 17:47:05 +0530

    adding a stack program

--- Solutions9.java
+++ Solutions9.java
@@ -16,7 +16,36 @@ public class Solutions9 {
         }
         return false;
     }
-    
+    public void removeCycle(ListNode head){
+        if(head ==null){
+            return;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(slow!=null && fast!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow ==fast){
+                break;
+            }
+        }
+        if(slow == null || fast == null){
+            return; // No cycle found
+        }
+
+        slow = head; // Reset slow to the start
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // Now 'slow' and 'fast' are at the start of the cycle
+        // To remove the cycle, we need to find the last node in the cycle
+        while(fast.next != slow){
+            fast = fast.next;
+        }
+        fast.next = null; // Break the cycle
+    }
 
     
 }
