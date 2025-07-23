eacec5df8b798b3ede0bf9cb8c35be5e91840e7b
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-07-23 18:13:51 +0530

    add a new program

--- StackClass.java
+++ StackClass.java
@@ -1,47 +1,13 @@
-
-import java.util.List;
+import java.util.*;
 
 public class StackClass {
-   static class Stack{
-        ArrayList<Integer> list=new ArrayList<>();
-        public static boolean isEmpty(){
-            return List.size()==0;
-        }
-        //push
-        public static void push(int data){
-            list.add(data);
-        }
-        //pop
-        public static int pop(){
-            if(isEmpty()){
-                System.out.println("Stack is empty");
-                return -1; // or throw an exception
-            }
-            int top =List.get()(List.size()-1);
-            List.remove(List.size()-1);
-            return top;
-        }
-        //peek
-        public static int peek(){
-            if(isEmpty()){
-                System.out.println("Stack is empty");
-                return -1; // or throw an exception
-            }
-            return List.get(List.size()-1);
-
-        }
-
-    }
-
-       public static void main(String args[]){
-        Stack s =new Stack();
+    public static void main(String args[]) {
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
-        while(!s.isEmpty()){
+        while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
         }
-
     }
-    
 }
