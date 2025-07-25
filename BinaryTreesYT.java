4f5c935bceb55dbcf4e184b7ca1ee1be3a31db7d
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-07-25 09:10:57 +0530

    program to count number of nodes

--- BinaryTreesYT.java
+++ BinaryTreesYT.java
@@ -80,11 +80,21 @@ public class BinaryTreesYT {
 
         }
     } 
+    public static int countnodes(Node root){
+        if(root == null){
+            return 0;
+
+        }
+        return 1 + countnodes(root.left) + countnodes(root.right);
+    }
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        levelorder(root);
+
+        System.out.println(countnodes(root));
+
+        
         
     }
     
