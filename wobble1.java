362b63f5f9e8bc22ff63ce89057e98acc1d43e35
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-07-28 10:08:37 +0530

    adding a intersection program

---
+++ wobble1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class wobble1 {
+    public static int intersection(int arr1[], int arr2[]) {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        for (int j = 0; j < arr2.length; j++) {
+            if (set.contains(arr2[j])) {
+                count++;
+                set.remove(arr2[j]);
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String args[]) {
+        int arr1[] = {1, 2, 3, 4, 5};
+        int arr2[] = {6, 7, 8, 9, 10};
+
+        System.out.println("Intersection of two arrays: " + intersection(arr1, arr2));
+    }
+}
